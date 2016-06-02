587994b49c0f3177a5ac9d0828495aa86482ed7f
svk31 <sigvekvalsvik@gmail.com>
2016-06-02 20:00:14 +0200

    Add examples folder with node.js examples

---
+++ examples/api.js
@@ -0,0 +1,13 @@
+// Node.js example
+
+var {Apis} = require("../");
+
+Apis.instance("wss://bitshares.openledger.info/ws").init_promise.then((res) => {
+    console.log("connected to:", res[0].network);
+
+    Apis.instance().db_api().exec( "set_subscribe_callback", [ updateListener, true ] )
+});
+
+function updateListener(object) {
+    console.log("set_subscribe_callback update:\n", object);
+}
---
+++ examples/chainStore.js
@@ -0,0 +1,15 @@
+var {Apis, ChainStore} = require("../");
+
+Apis.instance("wss://bitshares.openledger.info/ws").init_promise.then((res) => {
+    console.log("connected to:", res[0].network);
+    ChainStore.init().then(() => {
+        ChainStore.subscribe(updateState);
+    });
+});
+
+let dynamicGlobal = null;
+function updateState(object) {
+    dynamicGlobal = ChainStore.getObject("2.1.0");
+
+    console.log("ChainStore object update\n", dynamicGlobal ? dynamicGlobal.toJS() : dynamicGlobal);
+}
