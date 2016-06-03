1d544e2b769cd94f25386d5085815a471f2ce1e6
Sigve Kvalsvik <sigvekvalsvik@gmail.com>
2016-06-03 11:05:36 +0200

    Refactor ChainValidation and SerializerValidation

--- libraries/chain/src/ChainValidation.js
+++ libraries/chain/src/ChainValidation.js
@@ -6,125 +6,119 @@
     numbers, or well placed dashes.
     @see is_valid_name graphene/libraries/chain/protocol/account.cpp
 */
-function is_account_name(value, allow_too_short = false) {
-    var i, label, len, length, ref;
-    
-    if (is_empty(value)) {
-        return false;
-    }
-    
-    length = value.length;
-    
-    if ((!allow_too_short && length < 3) || length > 63) {
-        return false;
-    }
-    
-    ref = value.split('.');
-    
-    for (i = 0, len = ref.length; i < len; i++) {
+let id_regex = /\b\d+\.\d+\.(\d+)\b/;
+
+var chainValidation = {
+    is_account_name: function(value, allow_too_short = false) {
+        var i, label, len, length, ref;
         
-        label = ref[i];
-        
-        if (!(/^[a-z][a-z0-9-]*$/.test(label) && !/--/.test(label) && /[a-z0-9]$/.test(label))) {
+        if (this.is_empty(value)) {
             return false;
         }
         
-    }
-    return true;
-}
-
-let id_regex = /\b\d+\.\d+\.(\d+)\b/;
-
-function is_object_id(obj_id) {
-    if( 'string' != typeof obj_id )
-        return false
-    
-    let match = id_regex.exec(obj_id)
-    return (match !== null && obj_id.split(".").length === 3)
-}
-
-function is_empty(value) {
-    return value == null || value.length === 0
-}
-
-function is_account_name_error(value, allow_too_short) {
-    var i, label, len, length, ref, suffix;
-    if (allow_too_short == null) {
-        allow_too_short = false;
-    }
-    suffix = "Account name should ";
-    if (is_empty(value)) {
-        return suffix + "not be empty.";
-    }
-    length = value.length;
-    if (!allow_too_short && length < 3) {
-        return suffix + "be longer.";
-    }
-    if (length > 63) {
-        return suffix + "be shorter.";
-    }
-    if (/\./.test(value)) {
-        suffix = "Each account segment should ";
-    }
-    ref = value.split('.');
-    for (i = 0, len = ref.length; i < len; i++) {
-        label = ref[i];
-        if (!/^[~a-z]/.test(label)) {
-            return suffix + "start with a letter.";
+        length = value.length;
+        
+        if ((!allow_too_short && length < 3) || length > 63) {
+            return false;
         }
-        if (!/^[~a-z0-9-]*$/.test(label)) {
-            return suffix + "have only letters, digits, or dashes.";
+        
+        ref = value.split('.');
+        
+        for (i = 0, len = ref.length; i < len; i++) {
+            
+            label = ref[i];
+            
+            if (!(/^[a-z][a-z0-9-]*$/.test(label) && !/--/.test(label) && /[a-z0-9]$/.test(label))) {
+                return false;
+            }
+            
         }
-        if (/--/.test(label)) {
-            return suffix + "have only one dash in a row.";
+        return true;
+    },
+
+
+    is_object_id: function(obj_id) {
+        if( 'string' != typeof obj_id )
+            return false
+        
+        let match = id_regex.exec(obj_id)
+        return (match !== null && obj_id.split(".").length === 3)
+    },
+
+    is_empty: function(value) {
+        return value == null || value.length === 0
+    },
+
+    is_account_name_error: function(value, allow_too_short) {
+        var i, label, len, length, ref, suffix;
+        if (allow_too_short == null) {
+            allow_too_short = false;
         }
-        if (!/[a-z0-9]$/.test(label)) {
-            return suffix + "end with a letter or digit.";
+        suffix = "Account name should ";
+        if (this.is_empty(value)) {
+            return suffix + "not be empty.";
         }
-        if (!(label.length >= 3)) {
-            return suffix + "be longer";
+        length = value.length;
+        if (!allow_too_short && length < 3) {
+            return suffix + "be longer.";
         }
+        if (length > 63) {
+            return suffix + "be shorter.";
+        }
+        if (/\./.test(value)) {
+            suffix = "Each account segment should ";
+        }
+        ref = value.split('.');
+        for (i = 0, len = ref.length; i < len; i++) {
+            label = ref[i];
+            if (!/^[~a-z]/.test(label)) {
+                return suffix + "start with a letter.";
+            }
+            if (!/^[~a-z0-9-]*$/.test(label)) {
+                return suffix + "have only letters, digits, or dashes.";
+            }
+            if (/--/.test(label)) {
+                return suffix + "have only one dash in a row.";
+            }
+            if (!/[a-z0-9]$/.test(label)) {
+                return suffix + "end with a letter or digit.";
+            }
+            if (!(label.length >= 3)) {
+                return suffix + "be longer";
+            }
+        }
+        return null;
+    },
+        
+    is_cheap_name: function(account_name) {
+        return /[0-9-]/.test(account_name) || !/[aeiouy]/.test(account_name);
+    },
+
+    is_empty_user_input: function(value) {
+        if (this.is_empty(value)) { return true; }
+        if ((value+"").trim() === "") { return true; }
+        return false;
+    },
+
+    required: function(value, field_name="") {
+        if (this.is_empty(value)) {
+            throw new Error("value required for " + field_name + ": " +value);
+        }
+        return value;
+    },
+
+    /** @see is_valid_symbol graphene/libraries/chain/protocol/asset_ops.cpp */
+    is_valid_symbol_error: function(value) {
+        var suffix = "Asset name should ";
+        if (this.is_empty(value)) { return suffix + "not be empty."; }
+        if (value.split('.').length > 2) { return suffix + "have only one dot."; }
+        if (value.length < 3) { return suffix + "be longer."; }
+        if (value.length > 16) { return suffix + "be shorter."; }
+        if (!/^[A-Z]/.test(value)) { return suffix + "start with a letter"; }
+        if (!/[A-Z]$/.test(value)) { return suffix + "end with a letter"; }
+        if (/^[A-Z0-9\.]$/.test(value)) { return suffix + "contain only letters numbers and perhaps a dot."; }
+        return null;
     }
-    return null;
-}
-    
-function is_cheap_name(account_name){
-    return /[0-9-]/.test(account_name) || !/[aeiouy]/.test(account_name);
 }
 
-function is_empty_user_input(value){
-    if (is_empty(value)) { return true; }
-    if ((value+"").trim() === "") { return true; }
-    return false;
-}
-
-function required(value, field_name=""){
-    if (is_empty(value)) {
-        throw new Error(`value required for ${field_name}: ${value}`);
-    }
-    return value;
-}
-
-/** @see is_valid_symbol graphene/libraries/chain/protocol/asset_ops.cpp */
-function is_valid_symbol_error(value) {
-    var suffix = "Asset name should ";
-    if (is_empty(value)) { return suffix + "not be empty."; }
-    if (value.split('.').length > 2) { return suffix + "have only one dot."; }
-    if (value.length < 3) { return suffix + "be longer."; }
-    if (value.length > 16) { return suffix + "be shorter."; }
-    if (!/^[A-Z]/.test(value)) { return suffix + "start with a letter"; }
-    if (!/[A-Z]$/.test(value)) { return suffix + "end with a letter"; }
-    if (/^[A-Z0-9\.]$/.test(value)) { return suffix + "contain only letters numbers and perhaps a dot."; }
-    return null;
-}
-
-module.exports = {
-    is_account_name: is_account_name,
-    is_object_id: is_object_id,
-    is_empty: is_empty,
-    is_account_name_error: is_account_name_error,
-    is_cheap_name: is_cheap_name,
-    is_empty_user_input: is_empty_user_input,
-    required: required,
-    is_valid_symbol_error: is_valid_symbol_error
-};
+module.exports = chainValidation;
--- libraries/serializer/src/SerializerValidation.js
+++ libraries/serializer/src/SerializerValidation.js
@@ -1,21 +1,4 @@
-var _my;
-var is_empty;
-var is_digits;
-var to_number;
-var require_match;
-var require_object_id;
-var require_object_type;
-var get_instance;
-var require_relative_type;
-var get_relative_instance;
-var require_protocol_type;
-var get_protocol_instance;
-var get_protocol_type;
-var require_implementation_type;
-var get_implementation_instance;
 var Long = require('bytebuffer').Long;
-// var BigInteger = require('bigi');
-
 var ChainTypes = require('../../chain/src/ChainTypes');
 
 var MAX_SAFE_INT = 9007199254740991;
@@ -26,14 +9,14 @@ var MIN_SAFE_INT =-9007199254740991;
 
     Validations support a string format for dealing with large numbers.
 */
-module.exports = _my = {
+var _my = {
 
-    is_empty: is_empty=function(value){
+    is_empty: function(value){
         return value === null || value === undefined;
     },
     
     required(value, field_name=""){
-        if (is_empty(value) ){
+        if (this.is_empty(value) ){
             throw new Error(`value required ${field_name} ${value}`);
         }
         return value;
@@ -47,7 +30,7 @@ module.exports = _my = {
     },
     
     string(value){
-        if (is_empty(value) ){ return value; }
+        if (this.is_empty(value) ){ return value; }
         if (typeof value !== "string") {
             throw new Error(`string required: ${value}`);
         }
@@ -55,7 +38,7 @@ module.exports = _my = {
     },
     
     number(value){
-        if (is_empty(value) ){ return value; }
+        if (this.is_empty(value) ){ return value; }
         if (typeof value !== "number") {
             throw new Error(`number required: ${value}`);
         }
@@ -63,7 +46,7 @@ module.exports = _my = {
     },
     
     whole_number(value, field_name=""){
-        if (is_empty(value) ){ return value; }
+        if (this.is_empty(value) ){ return value; }
         if (/\./.test(value) ){
             throw new Error(`whole number required ${field_name} ${value}`);
         }
@@ -71,21 +54,21 @@ module.exports = _my = {
     },
     
     unsigned(value, field_name=""){
-        if (is_empty(value) ){ return value; }
+        if (this.is_empty(value) ){ return value; }
         if (/-/.test(value) ){
             throw new Error(`unsigned required ${field_name} ${value}`);
         }
         return value;
     },
     
-    is_digits: is_digits=function(value){
+    is_digits: function(value){
         if (typeof value === "numeric") { return true; }
         return /^[0-9]+$/.test(value);
     },
     
-    to_number: to_number=function(value, field_name=""){
-        if (is_empty(value) ){ return value; }
-        _my.no_overflow53(value, field_name);
+    to_number: function(value, field_name=""){
+        if (this.is_empty(value) ){ return value; }
+        this.no_overflow53(value, field_name);
         var int_value = (() => {
             if (typeof value === "number") {
                 return value;
@@ -97,10 +80,10 @@ module.exports = _my = {
     },
     
     to_long(value, field_name=""){
-        if (is_empty(value) ){ return value; }
+        if (this.is_empty(value) ){ return value; }
         if (Long.isLong(value) ){ return value; }
         
-        _my.no_overflow64(value, field_name);
+        this.no_overflow64(value, field_name);
         if (typeof value === "number") {
             value = ""+value;
         }
@@ -108,10 +91,10 @@ module.exports = _my = {
     },
     
     to_string(value, field_name=""){
-        if (is_empty(value) ){ return value; }
+        if (this.is_empty(value) ){ return value; }
         if (typeof value === "string") { return value; }
         if (typeof value === "number") {
-            _my.no_overflow53(value, field_name);
+            this.no_overflow53(value, field_name);
             return ""+value;
         }
         if (Long.isLong(value) ){
@@ -121,15 +104,15 @@ module.exports = _my = {
     },
     
     require_test(regex, value, field_name=""){
-        if (is_empty(value) ){ return value; }
+        if (this.is_empty(value) ){ return value; }
         if (!regex.test(value)) {
             throw new Error(`unmatched ${regex} ${field_name} ${value}`);
         }
         return value;
     },
     
-    require_match: require_match=function(regex, value, field_name=""){
-        if (is_empty(value) ){ return value; }
+    require_match: function(regex, value, field_name=""){
+        if (this.is_empty(value) ){ return value; }
         var match = value.match(regex);
         if (match === null) {
             throw new Error(`unmatched ${regex} ${field_name} ${value}`);
@@ -137,29 +120,29 @@ module.exports = _my = {
         return match;
     },
     
-    // require_object_id: require_object_id=function(value, field_name){
-    //     return require_match(
-    //         /^([0-9]+)\.([0-9]+)\.([0-9]+)$/,
-    //         value,
-    //         field_name
-    //     );
-    // },
+    require_object_id: function(value, field_name){
+        return require_match(
+            /^([0-9]+)\.([0-9]+)\.([0-9]+)$/,
+            value,
+            field_name
+        );
+    },
     
     // Does not support over 53 bits
     require_range(min,max,value, field_name=""){
-        if (is_empty(value) ){ return value; }
-        var number = to_number(value);
+        if (this.is_empty(value) ){ return value; }
+        var number = this.to_number(value);
         if (value < min || value > max) {
             throw new Error(`out of range ${value} ${field_name} ${value}`);
         }
         return value;
     },
     
-    require_object_type: require_object_type=function(
+    require_object_type: function(
         reserved_spaces = 1, type, value,
         field_name=""
     ){
-        if (is_empty(value) ){ return value; }
+        if (this.is_empty(value) ){ return value; }
         var object_type = ChainTypes.object_type[type];
         if (!object_type) {
             throw new Error(`Unknown object type ${type} ${field_name} ${value}`);
@@ -171,56 +154,56 @@ module.exports = _my = {
         return value;
     },
     
-    get_instance: get_instance=function(reserve_spaces, type, value, field_name){
-        if (is_empty(value) ){ return value; }
-        require_object_type(reserve_spaces, type, value, field_name);
-        return to_number(value.split('.')[2]);
+    get_instance: function(reserve_spaces, type, value, field_name){
+        if (this.is_empty(value) ){ return value; }
+        this.require_object_type(reserve_spaces, type, value, field_name);
+        return this.to_number(value.split('.')[2]);
     },
     
-    require_relative_type: require_relative_type=function(type, value, field_name){
-        require_object_type(0, type, value, field_name);
+    require_relative_type: function(type, value, field_name){
+        this.require_object_type(0, type, value, field_name);
         return value;
     },
     
-    get_relative_instance: get_relative_instance=function(type, value, field_name){
-        if (is_empty(value) ){ return value; }
-        require_object_type(0, type, value, field_name);
-        return to_number(value.split('.')[2]);
+    get_relative_instance: function(type, value, field_name){
+        if (this.is_empty(value) ){ return value; }
+        this.require_object_type(0, type, value, field_name);
+        return this.to_number(value.split('.')[2]);
     },
     
-    require_protocol_type: require_protocol_type=function(type, value, field_name){
-        require_object_type(1, type, value, field_name);
+    require_protocol_type: function(type, value, field_name){
+        this.require_object_type(1, type, value, field_name);
         return value;
     },
     
-    get_protocol_instance: get_protocol_instance=function(type, value, field_name){
-        if (is_empty(value) ){ return value; }
-        require_object_type(1, type, value, field_name);
-        return to_number(value.split('.')[2]);
+    get_protocol_instance: function(type, value, field_name){
+        if (this.is_empty(value) ){ return value; }
+        this.require_object_type(1, type, value, field_name);
+        return this.to_number(value.split('.')[2]);
     },
     
-    get_protocol_type: get_protocol_type=function(value, field_name){
-        if (is_empty(value) ){ return value; }
-        require_object_id(value, field_name);
+    get_protocol_type: function(value, field_name){
+        if (this.is_empty(value) ){ return value; }
+        this.require_object_id(value, field_name);
         var values = value.split('.');
-        return to_number(values[1]);
+        return this.to_number(values[1]);
     },
         
     get_protocol_type_name(value, field_name){
-        if (is_empty(value) ){ return value; }
-        var type_id = get_protocol_type(value, field_name);
+        if (this.is_empty(value) ){ return value; }
+        var type_id = this.get_protocol_type(value, field_name);
         return (Object.keys(ChainTypes.object_type))[type_id];
     },
     
-    require_implementation_type: require_implementation_type=function(type, value, field_name){
-        require_object_type(2, type, value, field_name);
+    require_implementation_type: function(type, value, field_name){
+        this.require_object_type(2, type, value, field_name);
         return value;
     },
     
-    get_implementation_instance: get_implementation_instance=function(type, value, field_name){
-        if (is_empty(value) ){ return value; }
-        require_object_type(2, type, value, field_name);
-        return to_number(value.split('.')[2]);
+    get_implementation_instance: function(type, value, field_name){
+        if (this.is_empty(value) ){ return value; }
+        this.require_object_type(2, type, value, field_name);
+        return this.to_number(value.split('.')[2]);
     },
 
     // signed / unsigned decimal
@@ -240,7 +223,7 @@ module.exports = _my = {
         }
         if (Long.isLong(value) ){
             // typeof value.toInt() is 'number'
-            no_overflow53(value.toInt(), field_name);
+            this.no_overflow53(value.toInt(), field_name);
             return;
         }
         throw `unsupported type ${field_name}: (${typeof value}) ${value}`;
@@ -253,7 +236,7 @@ module.exports = _my = {
         
         // BigInteger#isBigInteger https://github.com/cryptocoinjs/bigi/issues/20
         if (value.t !== undefined && value.s !== undefined) {
-            _my.no_overflow64(value.toString(), field_name);
+            this.no_overflow64(value.toString(), field_name);
             return;
         }
         
@@ -284,4 +267,6 @@ module.exports = _my = {
             
         throw `unsupported type ${field_name}: (${typeof value}) ${value}`;
     }
-    };
+};
+
+module.exports = _my;
